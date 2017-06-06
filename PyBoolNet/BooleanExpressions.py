d046c29ce3c0c7915b6964c8e54f647bce6f87c3
Sarah <sarah.nee@leer-online.de>
2017-06-06 13:42:39 +0200

    eqntott für mac

--- PyBoolNet/BooleanExpressions.py
+++ PyBoolNet/BooleanExpressions.py
@@ -99,11 +99,8 @@ def minimize_espresso(Expression, Outputfile=None, Merge=False, Equiv=False, Exa
     if Reduce == True:
         eqntott_cmd += ['-r']
 
-    cwd = os.getcwd()
-    os.chdir(os.path.normpath(os.path.join( BASE, "Dependencies/eqntott/bin")))
     eqntott_in = Expression
     eqntott_out = run_eqntott(eqntott_cmd, eqntott_in)
-    os.chdir(cwd)
 
     if int(re.search(r'\.p\s\d+', eqntott_out).group().strip(".p ")) != 0:
         espresso_out = run_espresso(espresso_cmd, eqntott_out)
